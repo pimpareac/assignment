a428e2a37d09c08501617be149d1a9a84bdb1586
Amol Pimpare <pimpareac@gmail.com>
2021-02-14 15:44:38 +0000

    Added List view controller. deletes unused code.

---
+++ CustomAppIcons/CustomAppIcons/UI/CustomIconList/CustomIconsListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CustomIconsListViewController.swift
+//  CustomAppIcons
+//
+//  Created by Amol Pimpare on 14/02/21.
+//
+
+import UIKit
+
+class CustomIconsListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }    
+}
+
+// MARK: - Table view data source
+
+extension CustomIconsListViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+}
